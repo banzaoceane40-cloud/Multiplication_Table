c52bae9e7b23bd2a2c8bfce3ca9c9ce6daddee73
banzaoceane40-cloud <banzaoceane40@gmail.com>
2026-04-01 16:58:35 +0200

    main activity linked

--- app/src/main/java/za/ac/iie/multiplicationtable/TableDisplay.kt
+++ app/src/main/java/za/ac/iie/multiplicationtable/TableDisplay.kt
@@ -1,6 +1,8 @@
 package za.ac.iie.multiplicationtable
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,17 @@ class TableDisplay : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_table_display)
+
+        val bundle: Bundle? = intent.extras
+        val tableString: String? = bundle?.getString("tableNumber")
+
+        //convert the table number to an integer
+        val tableNumber = tableString?.toInt()
+
+        //
+        val multiplicationTable = findViewById<TextView>(R.id.titleText)
+        multiplicationTable.text = "$tableNumber x table"
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
