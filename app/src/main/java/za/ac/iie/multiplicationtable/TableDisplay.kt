7193ef2cf960170302e01c3fadfd145f5e54f2c7
banzaoceane40-cloud <banzaoceane40@gmail.com>
2026-04-01 16:05:35 +0200

    added switch between activities

--- app/src/main/java/za/ac/iie/multiplicationtable/TableDisplay.kt
+++ app/src/main/java/za/ac/iie/multiplicationtable/TableDisplay.kt
@@ -1,26 +1,16 @@
 package za.ac.iie.multiplicationtable
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class TableDisplay : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val multiplyButton = findViewById<Button>(R.id.multiplyButton)
-        multiplyButton.setOnClickListener{
-            //create the explicit intent
-            val intent = Intent(this, TableDisplay::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_table_display)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
